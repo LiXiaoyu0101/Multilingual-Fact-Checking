f6709e5fc16025de82d2effbf26e11026e88ff32
X.Y.Li <32185935+LiXiaoyu0101@users.noreply.github.com>
2022-08-25 21:11:36 +0800

    Update init.d

--- x-fact-english/init.d
+++ x-fact-english/init.d
@@ -1,1 +1,10 @@
-
+Cite 
+@inproceedings{gupta2021xfact,
+      title={{X-FACT: A New Benchmark Dataset for Multilingual Fact Checking}}, 
+      author={Gupta, Ashim and Srikumar, Vivek},
+      booktitle = "Proceedings of the 59th Annual Meeting of the Association for Computational Linguistics",      
+      month = jul,
+      year = "2021",
+      address = "Online",
+      publisher = "Association for Computational Linguistics",
+}
